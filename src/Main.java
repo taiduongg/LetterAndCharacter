128e554408f51b9f3900f7621db78d75e903ac32
taiduongg <git.taidv1211@gmail.com>
2023-09-12 13:00:33 +0700

    init project

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+import controller.CountProgramming;
+
+/**
+ *
+ * @author ACER
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new CountProgramming().run();
+    }
+}
